d39769ce215eb897cbf77537ff14cbb62f83d0f7
Duncan MacIsaac <113464999+DjMacisaac@users.noreply.github.com>
2024-11-03 16:08:51 -0400

    Update ButtonComponent.java

---
+++ Code/ButtonComponent.java
@@ -0,0 +1,51 @@
+import javafx.scene.control.Button;
+
+public class ButtonComponent extends ViewComponent
+{
+    private Button thisObject;
+    private int id;
+    private int isClicked;
+
+    public ButtonComponent(int id)
+    {
+        //when clicked, controller will request button id and then
+        //translate this buttons id to the action required when this
+        //button is clicked
+        this.id=id;
+        isClicked=false;
+        thisObject = new Button();
+
+        // Buttons action on click
+        thisObject.setOnAction(event -> {
+            //MUST check if button is on topmost rank at its location.
+            //will probably make components which arent topmost rank at their location
+            //be hidden so keep that in mind.
+            isClicked = true;
+        });
+    }
+    @Override
+    protected void setHiddenHelper(boolean isHidden)
+    {
+        thisObject.setVisible(!isHidden);
+    }
+    @Override
+    protected void updateXYHelper(int[] xyCords)
+    {
+        thisObject.setLayoutX(xyCords[0]);
+        thisObject.setLayoutY(xyCords[2]);
+        thisObject.setPrefWidth(xyCords[1]-xyCords[0]);
+        thisObject.setPrefHeight(xyCords[3]-xyCords[2]);
+    }
+    //If clicked, then it has a message and once controller checks
+    //if this button has a message to send, it will request this buttons
+    //id to translate this message
+    public boolean hasMessage()
+    {
+        return isClicked;
+    }
+    public int getMessage()
+    {
+        isClicked = false;
+        return id;
+    }
+}
