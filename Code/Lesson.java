83ccc570927ec5e74301d1f75a7d84dc80c822e1
DanaeMorrison <156708590+DanaeMorrison@users.noreply.github.com>
2024-11-04 14:06:49 -0400

    Created Lesson.java

---
+++ Code/Lesson.java
@@ -0,0 +1,29 @@
+/**
+ * @author Danae Morrison
+ * @version 1.0
+ **/
+
+
+public class Lesson {
+    private int lessonID;
+    private String lessonInfo;
+    private Flashcard[] flashcardList;
+
+    public Lesson(int lessonID, String lessonInfo, Flashcard[] flashcardList) {
+        this.lessonID = lessonID;
+        this.lessonInfo = lessonInfo;
+        this.flashcardList = flashcardList;
+    }
+
+    public String getInfo() {
+        return lessonInfo;
+    }
+
+    public Flashcard[] getFlashcards() {
+        return flashcardList;
+    }
+
+    public int getLessonID() {
+        return lessonID;
+    }
+}
