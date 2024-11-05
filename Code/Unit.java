03df90096a0a4ee11673c87216d3672dfec359d2
DanaeMorrison <156708590+DanaeMorrison@users.noreply.github.com>
2024-11-05 13:24:07 -0400

    Developed Model and Unit Classes
    Model is a work in progress for sure. Going to start doing some tests

---
+++ Code/Unit.java
@@ -0,0 +1,28 @@
+/**
+ * @author Danae Morrison
+ * @version 1.0
+ **/
+
+public class Unit {
+    private int unitID;
+    private Lesson[] lessonList;
+    private String courseInfo;
+
+    public Unit(int unitID, Lesson[] lessonList, String courseInfo) {
+        this.unitID = unitID;
+        this.lessonList = lessonList;
+        this.courseInfo = courseInfo;
+    }
+
+    public String getInfo() {
+        return courseInfo;
+    }
+
+    public Lesson[] getLessons() {
+        return lessonList;
+    }
+
+    public int getUnitID() {
+        return unitID;
+    }
+}
