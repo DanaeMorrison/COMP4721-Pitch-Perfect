dba485c0c1f2e665eea051bd10192527f59f09be
jjrichard3 <113443944+jjrichard3@users.noreply.github.com>
2024-11-13 10:22:46 -0400

    Update Flashcard.java
    Changed it so that answer is an array so that we can handle chords. Also added a string for the clef and a char for the hand to help make a dynamic lesson view

--- Code/Model/Flashcard.java
+++ Code/Model/Flashcard.java
@@ -1,57 +1,44 @@
 package Model;
+
 /**
  * @author Danae Morrison
- * @version 1.0
+ * @version 1.1
  **/
-
 
 public class Flashcard {
     private int flashcardID;
-    private int imageID;
-    private int answer;
-    private int pointValue;
+    private int[] answer;
+    private String clef;
+    private char hand;
 
     // will be used during lessons and drills to determine
     // if a user has answered the flashcard correctly
-    private boolean isCorrect;
     // potentially a float value with the specific location
     // on an image component the whole note should be placed?
     // Or have the controller translate the answer value of the
     // flashcard to a float value that the image component can use
     // for placement?
 
-    public Flashcard(int flashcardID, int imageID, int answer, int pointValue) {
+    public Flashcard(int flashcardID, int[] answer, String clef, char hand) {
         this.flashcardID = flashcardID;
-        this.imageID = imageID;
         this.answer = answer;
-        this.pointValue = pointValue;
+        this.clef = clef;
+        this.hand = hand;
     }
 
     public int getID() {
         return flashcardID;
     }
 
-    public int getImageID() {
-        return imageID;
+    public char getHand() {
+        return hand;
     }
 
-    public int getAnswer() {
+    public int[] getAnswer() {
         return answer;
     }
 
-    public int getPointValue() {
-        return pointValue;
-    }
-
-    public void setFalse() {
-        this.isCorrect = false;
-    }
-
-    public void setTrue() {
-        this.isCorrect = true;
-    }
-
-    public boolean isCorrect() {
-        return isCorrect;
+    public String getClef() {
+        return clef;
     }
 }
