63742554925e41d0c6f5644b67ab7ec83c22903c
jjrichard3 <113443944+jjrichard3@users.noreply.github.com>
2024-11-25 13:06:15 -0400

    Update Model.java
    Finished Lessons for unit 1

--- Code/Model/Model.java
+++ Code/Model/Model.java
@@ -106,9 +106,9 @@ public class Model {
      */
     private void initializeUnit1() {
         // UNIT 1: Note Identification
-        Flashcard[] u1_l1 = new Flashcard[3];
-        Flashcard[] u1_l2 = new Flashcard[5];
-        Flashcard[] u1_l3 = new Flashcard[4];
+        Flashcard[] u1_l1 = new Flashcard[15];
+        Flashcard[] u1_l2 = new Flashcard[15];
+        Flashcard[] u1_l3 = new Flashcard[20];
         Flashcard[] u1_d1 = new Flashcard[6];
 
         Lesson[] unit1Lessons = new Lesson[3];
@@ -116,25 +116,66 @@ public class Model {
 
         // Lesson 1: Treble Clef Note Identification
         u1_l1[0] = new Flashcard(0, new int[] { 60 }, new int[] { 0 }, 'T', 'R'); // Middle C
-        u1_l1[1] = new Flashcard(0, new int[] { 64 }, new int[] { 4 }, 'T', 'R'); // E
-        u1_l1[2] = new Flashcard(0, new int[] { 67 }, new int[] { 7 }, 'T', 'R'); // G
-        unit1Lessons[0] = new Lesson(0, "Lesson 1", "Treble Clef Note Identification", u1_l1);
+        u1_l1[1] = new Flashcard(1, new int[] { 62 }, new int[] { 2 }, 'T', 'R'); // D
+        u1_l1[2] = new Flashcard(2, new int[] { 64 }, new int[] { 4 }, 'T', 'R'); // E
+        u1_l1[3] = new Flashcard(3, new int[] { 65 }, new int[] { 5 }, 'T', 'R'); // F
+        u1_l1[4] = new Flashcard(4, new int[] { 67 }, new int[] { 7 }, 'T', 'R'); // G
+        u1_l1[5] = new Flashcard(5, new int[] { 72 }, new int[] { 12 }, 'T', 'R'); // C
+        u1_l1[6] = new Flashcard(6, new int[] { 60 }, new int[] { 7 }, 'T', 'R'); // Middle C
+        u1_l1[7] = new Flashcard(7, new int[] { 71 }, new int[] { 11 }, 'T', 'R'); // B
+        u1_l1[8] = new Flashcard(8, new int[] { 69 }, new int[] { 9 }, 'T', 'R'); // A
+        u1_l1[9] = new Flashcard(9, new int[] { 65 }, new int[] { 5 }, 'T', 'R'); // F
+        u1_l1[10] = new Flashcard(10, new int[] { 67 }, new int[] { 7 }, 'T', 'R'); // G
+        u1_l1[11] = new Flashcard(11, new int[] { 62 }, new int[] { 2 }, 'T', 'R'); // D
+        u1_l1[12] = new Flashcard(12, new int[] { 69 }, new int[] { 9 }, 'T', 'R'); // A
+        u1_l1[13] = new Flashcard(13, new int[] { 64 }, new int[] { 4 }, 'T', 'R'); // E
+        u1_l1[14] = new Flashcard(14, new int[] { 72 }, new int[] { 12 }, 'T', 'R'); // C
+
+        unit1Lessons[0] = new Lesson(0, "Lesson 1", "Introduction to Treble Clef Note Identification", u1_l1);
         lessons.add(unit1Lessons[0]);
 
         // Lesson 2: Bass Clef Note Identification
-        u1_l2[0] = new Flashcard(0, new int[] { 43 }, new int[] { 0 }, 'B', 'L'); // F
-        u1_l2[1] = new Flashcard(0, new int[] { 46 }, new int[] { 3 }, 'B', 'L'); // A
-        u1_l2[2] = new Flashcard(0, new int[] { 48 }, new int[] { 5 }, 'B', 'L'); // C
-        u1_l2[3] = new Flashcard(0, new int[] { 50 }, new int[] { 7 }, 'B', 'L'); // D
-        u1_l2[4] = new Flashcard(0, new int[] { 52 }, new int[] { 9 }, 'B', 'L'); // E
+        u1_l2[0] = new Flashcard(0, new int[] { 53 }, new int[] { 5 }, 'B', 'L'); // F
+        u1_l2[1] = new Flashcard(1, new int[] { 55 }, new int[] { 7 }, 'B', 'L'); // G
+        u1_l2[2] = new Flashcard(2, new int[] { 57 }, new int[] { 9 }, 'B', 'L'); // A
+        u1_l2[3] = new Flashcard(3, new int[] { 59 }, new int[] { 11 }, 'B', 'L'); // B
+        u1_l2[4] = new Flashcard(4, new int[] { 60 }, new int[] { 0 }, 'B', 'L'); // Middle C
+        u1_l2[5] = new Flashcard(5, new int[] { 48 }, new int[] { 12 }, 'B', 'L'); // C
+        u1_l2[6] = new Flashcard(6, new int[] { 52 }, new int[] { 2 }, 'B', 'L'); // E
+        u1_l2[7] = new Flashcard(7, new int[] { 55 }, new int[] { 0 }, 'B', 'L'); // G
+        u1_l2[8] = new Flashcard(8, new int[] { 50 }, new int[] { 7 }, 'B', 'L'); // D
+        u1_l2[9] = new Flashcard(9, new int[] { 48 }, new int[] { 5 }, 'B', 'L'); // C
+        u1_l2[10] = new Flashcard(10, new int[] { 57 }, new int[] { 12 }, 'B', 'L'); // A
+        u1_l2[11] = new Flashcard(11, new int[] { 59 }, new int[] { 9 }, 'B', 'L'); // B
+        u1_l2[12] = new Flashcard(12, new int[] { 53 }, new int[] { 3 }, 'B', 'L'); // F
+        u1_l2[13] = new Flashcard(13, new int[] { 43 }, new int[] { 2 }, 'B', 'L'); // G
+        u1_l2[14] = new Flashcard(14, new int[] { 47 }, new int[] { 0 }, 'B', 'L'); // B
+
         unit1Lessons[1] = new Lesson(1, "Lesson 2", "Bass Clef Note Identification", u1_l2);
         lessons.add(unit1Lessons[1]);
 
         // Lesson 3: Mixed Treble and Bass Clef Notes
         u1_l3[0] = new Flashcard(0, new int[] { 60 }, new int[] { 0 }, 'T', 'R'); // Middle C (Treble)
-        u1_l3[1] = new Flashcard(0, new int[] { 43 }, new int[] { 0 }, 'B', 'L'); // F (Bass)
-        u1_l3[2] = new Flashcard(0, new int[] { 64 }, new int[] { 4 }, 'T', 'R'); // E (Treble)
-        u1_l3[3] = new Flashcard(0, new int[] { 46 }, new int[] { 3 }, 'B', 'L'); // A (Bass)
+        u1_l3[1] = new Flashcard(1, new int[] { 57 }, new int[] { 0 }, 'T', 'R'); // A (Treble)
+        u1_l3[2] = new Flashcard(2, new int[] { 52 }, new int[] { 4 }, 'B', 'L'); // E (Bass)
+        u1_l3[3] = new Flashcard(3, new int[] { 47 }, new int[] { 3 }, 'B', 'L'); // B (Bass)
+        u1_l3[4] = new Flashcard(4, new int[] { 65 }, new int[] { 7 }, 'T', 'R'); // F (Treble)
+        u1_l3[5] = new Flashcard(5, new int[] { 74 }, new int[] { 5 }, 'T', 'R'); // D (Treble)
+        u1_l3[6] = new Flashcard(6, new int[] { 60 }, new int[] { 0 }, 'B', 'L'); // Middle C (Bass) //
+        u1_l3[7] = new Flashcard(7, new int[] { 45 }, new int[] { 0 }, 'B', 'L'); // A (Bass)
+        u1_l3[8] = new Flashcard(8, new int[] { 76 }, new int[] { 4 }, 'T', 'R'); // E (Treble)
+        u1_l3[9] = new Flashcard(9, new int[] { 79 }, new int[] { 3 }, 'T', 'R'); // G (Treble)
+        u1_l3[10] = new Flashcard(10, new int[] { 59 }, new int[] { 7 }, 'T', 'R'); // B (Bass)
+        u1_l3[11] = new Flashcard(11, new int[] { 40 }, new int[] { 5 }, 'B', 'L'); // E (Bass)
+        u1_l3[12] = new Flashcard(12, new int[] { 60 }, new int[] { 0 }, 'T', 'R'); // Middle C (Treble)
+        u1_l3[13] = new Flashcard(13, new int[] { 43 }, new int[] { 0 }, 'B', 'L'); // F (Bass)
+        u1_l3[14] = new Flashcard(14, new int[] { 64 }, new int[] { 4 }, 'T', 'R'); // E (Treble)
+        u1_l3[15] = new Flashcard(15, new int[] { 46 }, new int[] { 3 }, 'B', 'L'); // A (Bass)
+        u1_l3[16] = new Flashcard(16, new int[] { 67 }, new int[] { 7 }, 'T', 'R'); // G (Treble)
+        u1_l3[17] = new Flashcard(17, new int[] { 48 }, new int[] { 5 }, 'B', 'L'); // C (Bass)
+        u1_l3[18] = new Flashcard(18, new int[] { 60 }, new int[] { 0 }, 'T', 'R'); // Middle C (Treble)
+        u1_l3[19] = new Flashcard(19, new int[] { 43 }, new int[] { 0 }, 'B', 'L'); // F (Bass)
+
         unit1Lessons[2] = new Lesson(2, "Lesson 3", "Mixed Treble and Bass Clef Notes", u1_l3);
         lessons.add(unit1Lessons[2]);
 
